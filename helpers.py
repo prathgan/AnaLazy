e37e1df46abca07f45a398ef3e7b623c1bec4b24
prathgan <gandhips02@gmail.com>
2020-05-13 17:51:55 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,8 +1,2 @@
 def get_non_features(column_headers, feature_names):
 	return list(set(column_headers) - set(feature_names))
-
-def update_session(updated_vars):
-	return
-
-def extract_training_params(request):
-	return request

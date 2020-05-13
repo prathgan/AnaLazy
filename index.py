53507d77993fcee9242931697a973e843f4e7f0d
prathgan <gandhips02@gmail.com>
2020-05-13 17:22:26 -0400

    Update index.py

--- index.py
+++ index.py
@@ -66,7 +66,7 @@ def upload():
 
 @app.route('/train', methods=['POST', 'GET'])
 def train():
-    # print(request.form)
+    print(request.form)
     # print(session)
     try:
         if session['filechoice'] != None:

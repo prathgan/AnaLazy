484320b126cc8719f563d9b2329579ca059b3614
prathgan <gandhips02@gmail.com>
2020-05-15 19:26:40 -0400

    add get_models_list

--- page_fns/models_fns.py
+++ page_fns/models_fns.py
@@ -2,3 +2,14 @@ import os
 from os import listdir
 from os.path import isfile, join
 import sys
+
+def get_models_list():
+    path = "models/"
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    try:
+        files.remove('.DS_Store')
+    except:
+        pass
+    for f in files:
+        f = f[:-7]
+    return files
